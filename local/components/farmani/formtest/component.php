a543e21343ee7120dc668bd27be7b356e0b0155d
Вадим <vadimoblov@gmail.com>
2023-03-19 13:29:17 +0300

    Полформы2_

--- local/components/farmani/formtest/component.php
+++ local/components/farmani/formtest/component.php
@@ -1,5 +1,5 @@
 <? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-    $arResult['CATEGORY'] = array('Фильтры', 'Шины');
+    $arResult['CATEGORY'] = array('Фильтры', 'Шины', 'Колеса');
     $arResult['REQUEST_TYPE'] = array('Запрос цены и сроков поставки', 'Пополнение складов');
     $arResult['STOCK_HOUSE'] = array('Склад 1', 'Склад 2', 'Склад 3');
     $this->IncludeComponentTemplate();
